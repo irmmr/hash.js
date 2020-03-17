4d9231eb929b9a4ee91433cb4a48f4ac0969e9a4
mehdi <irmmr.ir@gmail.com>
2020-03-17 10:26:30 +0330

    fix index

--- index.js
+++ index.js
@@ -16,25 +16,72 @@ var lib = new Hash.lib(),
     inf = new Hash.info(),
     adn = inf.addons;
 
-// if Hash.spa is ready
-if (adn.spa) {
-    
-    var spaApp = new Hash.spa();
-    spaApp.router({
-        router : 'go/{any}',
-        do : function(e) {
+if (adn.load) {
 
-            //get go_id
-            var go = e[0],
-                el = document.getElementById('handler_' + go);
+    var lo = new Hash.load(),
+        le = document.getElementById('loading');
 
-            if (el !== null) {
-                el.scrollIntoView();
-            } else {
-                lib.clear();
-            }
-            
+    lo.page({
+        load : function() {
+            le.innerHTML = '<img src="demo/loading.svg">';
+        },
+        do : function() {
+            le.style.marginTop = '-228px';
+            var st = setTimeout(function() {
+                le.style.display = 'none';
+                window.clearTimeout(st);
+            }, 500);
         }
     });
 
 }
+
+function addQ(q) {
+    for (var i in q) {
+        if (!lib.have(i, 'query')) {
+
+            var myQ = {};
+            myQ[i] = q[i];
+
+            lib.add({
+                query : myQ
+            });
+        }
+    }
+}
+
+function showNavs() {
+       
+    // query name => #nav=?
+    var nav = 'nav';
+    
+    // check if page have `nav` query
+    if (lib.have(nav, 'query')) {
+        // if query is not null => !#nav
+        if (!lib.is(null, { query : nav })) {
+            
+            //get 'nav' query
+            var nav_val = lib.get(true, {
+                query : nav
+            })[nav];
+            
+            //get element => myElem : elm_$
+            var elm = document.getElementById('nav_' + nav_val);
+            
+            if (elm !== null) {
+                elm.scrollIntoView();
+            } else {
+                lib.remove({
+                    query : [nav]
+                });
+            }
+            
+        } else {
+            lib.remove({
+                query : [nav]
+            });
+        }
+    }
+}
+
+showNavs(), Hash.event('change', showNavs);
