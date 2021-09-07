cddfd4a13c5bece11c283c5e2ec1d0952f78c813
irmmr <irmmr.ir@gmail.com>
2021-09-07 10:38:23 +0430

    add log, window configs

--- src/hash.js
+++ src/hash.js
@@ -1,11 +1,17 @@
 import HashComponent from './component'
 
-// define all hash components into main handle
-const Hash = new HashComponent({
+// default configs
+const default_configs = {
     getHashCallback: null,
     setHashCallback: null,
     getHashFilter: null,
-    setHashFilter: null
-})
+    setHashFilter: null,
+    getHrefCallback: null,
+    window: null,
+    log: true
+}
+
+// define all hash components into main handle
+const Hash = new HashComponent(default_configs)
 
 export default Hash
