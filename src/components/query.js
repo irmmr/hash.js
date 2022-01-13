9ebd55cd8e74b723f0ef249eabe4664f02bc5dcf
irmmr <irmmr.ir@gmail.com>
2022-01-13 19:12:12 +0330

    divide all methods

--- src/components/query.js
+++ src/components/query.js
@@ -1,52 +1,17 @@
 import HashComponent from "../component.js"
-import {getQuery, getTrueHash, getWinHash, isEmpty, isQueParOk, isString, setWinHash, toQuery} from "../helpers.js"
+import {HashCpQuery} from "./holder.js"
 
-/**
- * update a query value in location hash.
- * @param {string} n
- * @param {string|null|number} e
- * @returns boolean
- */
-HashComponent.updateQuery = (n, e) => {
-    if (!isString(n) || isEmpty(n) || !isQueParOk(e)) {
-        return false
-    }
+import "./query/add.js"
+import "./query/clear.js"
+import "./query/define.js"
+import "./query/get.js"
+import "./query/have.js"
+import "./query/is.js"
+import "./query/set.js"
+import "./query/update.js"
+import "./query/remove.js"
 
-    if (e === undefined) {
-        return false
-    }
+import "./query/str.js"
 
-    let wh      = getWinHash(),
-        hash    = getTrueHash(wh),
-        hsh_val = hash[0],
-        hsh_que = hash[1],
-        vl      = '',
-        cl      = {},
-        ch      = 0
-
-    if (isEmpty(hsh_que)) {
-        return false
-    }
-
-    if (!isEmpty(hsh_val)) {
-        vl += hsh_val
-    }
-
-    let que = getQuery(hsh_que), i
-
-    for (i in que) {
-        if (!que.hasOwnProperty(i)) {
-            continue
-        }
-        if (i === n) {
-            cl[i] = e
-            ch ++
-        } else {
-            cl[i] = que[i]
-        }
-    }
-
-    vl += '?' + toQuery(cl)
-    setWinHash(vl)
-    return ch !== 0
-}
+HashComponent.query = HashCpQuery
+HashComponent.q     = HashCpQuery
---
+++ src/components/value.js
@@ -0,0 +1,14 @@
+import HashComponent from "../component.js"
+import {HashCpValue} from "./holder.js"
+
+import "./value/add.js"
+import "./value/clear.js"
+import "./value/get.js"
+import "./value/have.js"
+import "./value/is.js"
+import "./value/set.js"
+import "./value/replace.js"
+import "./value/remove.js"
+
+HashComponent.value = HashCpValue
+HashComponent.v     = HashCpValue
