426ccd1734d84ae4ba0e3583090a1ecd8f530d87
irmmr <irmmr.ir@gmail.com>
2022-01-13 19:05:42 +0330

    update messages

--- src/message.js
+++ src/message.js
@@ -1,4 +1,4 @@
 export default {
-    event_und: '"addEventListener" does not exist in the "window" that you defined.',
-    win_problem: 'The "window" you defined in configs has some problems.'
+    event_und: '"addEventListener" does not exist in the "window" that you defined',
+    win_problem: 'The "window" you defined in configs has some problems'
 }
