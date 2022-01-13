b58d4c9c50032716555bd45d32ab2a12e3f36da6
irmmr <irmmr.ir@gmail.com>
2022-01-13 19:05:08 +0330

    update main vars

--- src/vars.js
+++ src/vars.js
@@ -1,3 +1,7 @@
-export const empty_object = Object.freeze({})
+export const empty_object   = Object.freeze({})
 
-export const empty_func   = () => {}
+export const empty_func     = function () {}
+
+export const and_symbol     = '&'
+export const equ_symbol     = '='
+export const que_symbol     = '?'
