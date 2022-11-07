4bd98eea5251a307da7103ee46e97db0f17d4afb
irmmr <irmmr.ir@gmail.com>
2022-11-07 20:56:13 +0330

    redirect to other components

--- src/components/query.js
+++ src/components/query.js
@@ -1,5 +1,5 @@
 import HashComponent from "../component.js";
-import {HashCpQuery} from "./holder.js";
+import {HashCpQuery, HashCpValue} from "./holder.js";
 
 import "./query/add.js";
 import "./query/clear.js";
@@ -12,6 +12,14 @@ import "./query/update.js";
 import "./query/remove.js";
 
 import "./query/str.js";
+
+/**
+ * Create a component that is a way
+ * to main component and a way to other
+ * components for using in one-line mode
+ */
+HashCpQuery.main  = HashCpQuery.m = HashComponent;
+HashCpQuery.value = HashCpQuery.v = HashCpValue;
 
 /**
  * Query components
--- src/components/value.js
+++ src/components/value.js
@@ -1,5 +1,5 @@
 import HashComponent from "../component.js";
-import {HashCpValue} from "./holder.js";
+import {HashCpValue, HashCpQuery} from "./holder.js";
 
 import "./value/add.js";
 import "./value/clear.js";
@@ -9,6 +9,14 @@ import "./value/is.js";
 import "./value/set.js";
 import "./value/replace.js";
 import "./value/remove.js";
+
+/**
+ * Create a component that is a way
+ * to main component and a way to other
+ * components for using in one-line mode
+ */
+HashCpValue.main  = HashCpValue.m = HashComponent;
+HashCpValue.query = HashCpValue.q = HashCpQuery;
 
 /**
  * Value components
