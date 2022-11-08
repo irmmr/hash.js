6a9e80a196a0eadee6b90172b06875328327f8e5
irmmr <irmmr.ir@gmail.com>
2022-11-08 23:50:25 +0330

    rw class with ES6 and remove helpers

--- src/config.js
+++ src/config.js
@@ -1,52 +1,18 @@
 import {empty_object, and_symbol, equ_symbol, que_symbol} from "./vars.js";
 
 /**
- * (configs helper)
- * object check.
- *
- * @param   item
- * @returns {boolean}
- */
-const _isObj = (item) => (item && typeof item === 'object' && item.constructor === Object);
-
-/**
- * (configs helper)
- * Deep merge two objects.
- *
- * @param   {object}    target
- * @param   {object}    source
- * @returns {object}
- */
-const _deepMerge = (target, source) => {
-    if (!_isObj(target) || !_isObj(source)) {
-        return target;
-    }
-
-    for (const key in source) {
-        if (_isObj(source[key])) {
-            if (!target[key]) Object.assign(target, {
-                [key]: {}
-            });
-
-            _deepMerge(target[key], source[key]);
-        } else {
-            Object.assign(target, {
-                [key]: source[key]
-            });
-        }
-    }
-
-    return target;
-}
-
-/**
  * HashConfig class.
  */
 class HashConfig {
     /**
+     * define all default config data
+     */
+    static #configs = HashConfig.defaults();
+
+    /**
      * Get all default configs
      *
-     * @returns {{andSymbol: string, log: boolean, setHashCallback: null, window: null, queSymbol: string, getHashFilter: null, setHashFilter: null, equSymbol: string, getHashCallback: null, getHrefCallback: null}}
+     * @returns {object}
      */
     static defaults() {
         return {
@@ -98,7 +64,7 @@ class HashConfig {
      * @returns {boolean}
      */
     static has(name) {
-        return typeof HashConfig.configs === 'object' && name in HashConfig.configs;
+        return typeof HashConfig.#configs === 'object' && name in HashConfig.#configs;
     }
 
     /**
@@ -111,7 +77,7 @@ class HashConfig {
             return;
         }
 
-        HashConfig.configs = options;
+        HashConfig.#configs = options;
     }
 
     /**
@@ -123,8 +89,8 @@ class HashConfig {
             return;
         }
 
-        let configs = HashConfig.configs;
-        HashConfig.define(Object.assign(configs, options));
+        let configs = HashConfig.#configs;
+        HashConfig.define(Object.assign({}, configs, options));
     }
 
     /**
@@ -135,7 +101,7 @@ class HashConfig {
      * @returns {string|Readonly<{}>|*}
      */
     static get(name = null, def = '') {
-        let configs = HashConfig.configs;
+        let configs = HashConfig.#configs;
 
         if (null == name) {
             return configs;
@@ -172,22 +138,5 @@ class HashConfig {
         return def;
     }
 }
-
-/**
- * Get config helpers.
- *
- * @returns {{isObj: (function(*)), deepMerge: (function(*, *): *)}}
- */
-HashConfig.h = {
-    isObj: _isObj,
-    deepMerge: _deepMerge
-}
-
-/**
- * Main config holder.
- *
- * @type {{andSymbol: string, log: boolean, setHashCallback: null, window: null, queSymbol: string, getHashFilter: null, setHashFilter: null, equSymbol: string, getHashCallback: null, getHrefCallback: null}}
- */
-HashConfig.configs = HashConfig.defaults();
 
 export default HashConfig
